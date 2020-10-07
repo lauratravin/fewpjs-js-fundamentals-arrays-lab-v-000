a3e35d597c33b811f90bf885df3d45327c39ede1
Laura Messa Travin <laura.travin@gmail.com>
2020-10-07 21:52:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution here!
+const append =["Milo", "Otis", "Garfield"];
+// Write your solution here!
+const prepend =["Milo", "Otis", "Garfield"];
+// Write your solution here!
+const removeLast =["Milo", "Otis", "Garfield"];
+// Write your solution here!
+const removeFirst =["Milo", "Otis", "Garfield"];
